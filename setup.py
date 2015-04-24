cbf11a6de3eb23009b107ada1bb2fd9f61afc788
Evan Fagerberg <efagerberg@safaribooksonline.com>
2015-04-24 15:09:07 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='sbo_validate_email',
-      version = '1.03',
+      version = '1.04',
       download_url = 'git@github.com:sefagerberg/sbo_validate_email.git',
       py_modules = ('sbo_validate_email',),
       author = 'Evan Fagerberg',
