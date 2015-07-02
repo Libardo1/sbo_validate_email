2930e1ab8acb315d652f034b2ea2bd24fa4be461
Evan Fagerberg <efagerberg@safaribooksonline.com>
2015-07-02 11:19:18 -0400

    master | add changelog and update setup/readme

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='sbo_validate_email',
-      version = '1.05',
-      download_url = 'git@github.com:sefagerberg/sbo_validate_email.git',
+      version = '1.06',
+      download_url = 'git@github.com:safarijv/sbo_validate_email.git',
       py_modules = ('sbo_validate_email',),
       author = 'Evan Fagerberg',
       author_email = 'efagerberg@safaribooksonline.com',
       description = 'validate_email verifies if an email address is valid and really exists.',
       long_description=open('README.rst').read(),
       keywords = 'email validation verification mx verify',
-      url = 'http://github.com/efagerberg/sbo_validate_email',
+      url = 'http://github.com/safarijv/sbo_validate_email',
       license = 'LGPL',
     )
